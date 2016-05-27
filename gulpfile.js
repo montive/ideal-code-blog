f609179d6484c7ab0a8124d9334f5c30c471132d
Dimitrio Mandamadiotis <dimitriosmandamadiotis@gmail.com>
2016-05-27 18:59:26 -0400

    Agregado css minificado

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
 var shell = require('gulp-shell')
+var cleanCSS = require('gulp-clean-css');
 
 var stylus = require('gulp-stylus');
 
@@ -20,6 +21,15 @@ var paths = {
 
 gulp.task('watch:stylus', function () {
 	gulp.watch(paths.style.all, ['stylus']);
+});
+
+gulp.task('minify-css', function() {
+    return gulp.src('./public/styles/*.css')
+        .pipe(cleanCSS({debug: true}, function(details) {
+            console.log(details.name + ': ' + details.stats.originalSize);
+            console.log(details.name + ': ' + details.stats.minifiedSize);
+        }))
+        .pipe(gulp.dest('./public/dist'));
 });
 
 gulp.task('stylus', function () {
