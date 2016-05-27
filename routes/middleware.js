3f5878bbe6e2a3fb0f8c1171ee9e39e38c08f2d3
Dimitrio Mandamadiotis <dimitriosmandamadiotis@gmail.com>
2016-05-27 13:11:06 -0400

    Agregado redirect de www a non www

--- routes/middleware.js
+++ routes/middleware.js
@@ -55,3 +55,14 @@ exports.requireUser = function (req, res, next) {
 		next();
 	}
 };
+
+/**
+	Redirects www to non www
+	*/
+	exports.wwwRedirect = function (req, res, next) {
+	    if (req.headers.host.slice(0, 4) === 'www.') {
+	        var newHost = req.headers.host.slice(4);
+	        return res.redirect(301, req.protocol + '://' + newHost + req.originalUrl);
+	    }
+	    next();
+	};
