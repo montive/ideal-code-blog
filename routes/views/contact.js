e0cbc95172d7525b26e87dc989f03fae87c1af41
Dimitrio Mandamadiotis <dimitriosmandamadiotis@gmail.com>
2016-05-29 18:48:13 -0400

    Modificado formulario y otros detalles

--- routes/views/contact.js
+++ routes/views/contact.js
@@ -22,7 +22,7 @@ exports = module.exports = function (req, res) {
 		updater.process(req.body, {
 			flashErrors: true,
 			fields: 'name, email, phone, enquiryType, message',
-			errorMessage: 'There was a problem submitting your enquiry:',
+			errorMessage: 'Hubo un problema al enviar su mensaje:',
 		}, function (err) {
 			if (err) {
 				locals.validationErrors = err.errors;
